13c930ddecc8c29c2be3ead54bbed0355f8318aa
Andrea Villegas <andrea@villegas.com>
2020-04-11 02:28:09 -0400

    controller added for forumPost

---
+++ HospitalProjectTeam4/Controllers/ForumPostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+//required for SqlParameter class
+using System.Data.SqlClient;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HospitalProjectTeam4.Data;
+using HospitalProjectTeam4.Models;
+using HospitalProjectTeam4.Models.ViewModels;
+using System.Diagnostics;
+using System.IO;
+//needed for other sign in feature classes
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace HospitalProjectTeam4.Controllers
+{
+    public class ForumPostController : Controller
+    {
+        private HospitalProjectContext db = new HospitalProjectContext();
+        // GET: ForumPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HospitalProjectTeam4/Models/ViewModels/ForumPostDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace HospitalProjectTeam4.Models.ViewModels
+{
+    public class ForumPostDetails
+    {
+        //Information about the posting
+
+        //List of all the replies
+    }
+}
