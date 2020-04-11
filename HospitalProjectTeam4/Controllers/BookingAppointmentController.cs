3ce77a7af1b791362d6d85aa8e5450aa2c77a632
Zameer Chariwala <zmrchariwala@gmail.com>
2020-04-11 16:02:29 -0400

    Feature 1

--- HospitalProjectTeam4/Controllers/BookingAppointmentController.cs
+++ HospitalProjectTeam4/Controllers/BookingAppointmentController.cs
@@ -36,15 +36,18 @@ namespace HospitalProjectTeam4.Controllers
         [HttpPost]
         public ActionResult Add(int doctorid,string datebooking,int patientid)
         {
-            Debug.WriteLine(doctorid + datebooking + patientid);
+            //Debug.WriteLine(doctorid + datebooking + patientid);
             //string BookingTime = "1200";
+            DateTime now = DateTime.Now;
+            Debug.WriteLine(now);
             Booking book = new Booking();
             book.DoctorID = doctorid;
+            book.CurrentDate = now.ToString();
             book.BookingDate = datebooking;
             book.PatientID = patientid;
             db.Bookings.Add(book);
             db.SaveChanges();
-
+            //return View();
             return RedirectToAction("List");
         }
 
@@ -53,6 +56,47 @@ namespace HospitalProjectTeam4.Controllers
             List<Booking> Bookings;
             Bookings = db.Bookings.ToList();
             return View(Bookings);
+        }
+        
+        public ActionResult Show(int id)
+        {
+
+
+            Booking booking = db.Bookings.FirstOrDefault(b => b.BookingID == id);
+            Debug.WriteLine(id);
+            return View(booking);
+        }
+        public ActionResult Update(int id)
+        {
+            UpdateBooking viewmodel = new UpdateBooking();
+
+            viewmodel.Booking = db.Bookings.FirstOrDefault(booking => booking.BookingID == id);
+            viewmodel.Doctors = db.Doctors.ToList();
+            viewmodel.Patients = db.Patients.ToList();
+
+            return View(viewmodel);
+        }
+        [HttpPost]
+        public ActionResult Update(int id, int doctorid, string datebooking, int patientid)
+        {
+            
+
+            Booking booking = db.Bookings.FirstOrDefault(b => b.BookingID == id);
+
+            booking.DoctorID = doctorid;
+            booking.BookingDate = datebooking;
+            booking.PatientID = patientid;
+
+            db.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+        public ActionResult Delete(int id)
+        {
+            string query = "delete from Bookings where BookingID = @id";
+            SqlParameter param = new SqlParameter("@id", id);
+            db.Database.ExecuteSqlCommand(query, param);
+            return RedirectToAction("List");
         }
     }
 }
